654b45a1e7dc3120828cd050a1938c5b62ce85b2
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-19 20:38:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by { |s| s.length }
+end
+
+def swap_elements(arr)
+end
+
+def reverse_array(arr)
+end
+
+def kesha_maker(arr)
+end
+
+def find_a(arr)
+end
+
+def sum_array(arr)
+end
+
+def add_s(arr)
+end
